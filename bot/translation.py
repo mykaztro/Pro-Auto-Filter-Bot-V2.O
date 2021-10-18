17c84ec5cbdbc9a4fc0f1395d2e6d9bb210aa44f
mykaztro <87994885+mykaztro@users.noreply.github.com>
2021-10-19 03:50:41 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -28,7 +28,7 @@ I'm an Autofilter bot v2.0 and you can not Add Me to your Group. I was made for 
             
    ☞ <code>Status</code> - <b>Button Will Shows The Stats Of Your Channel</b>
         
-<b><a href="https://t.me/Mo_Tech_YT">© Mo Tech YT</a></b>
+<b><a href="https://t.me/Sanoob_Achu_18">© Achu Vj</a></b>
 """
     
     ABOUT_TEXT = """<b>☞ 📃Name</b> : <b>Pro Auto Filter Bot V2.O</b>
@@ -37,7 +37,7 @@ I'm an Autofilter bot v2.0 and you can not Add Me to your Group. I was made for 
 
 <b>☞ 📚Library</b> : Pyrogram Asyncio 1.13.0
 
-<b>☞ 📖Source Code</b> : <a href="https://github.com/MoTechYT/Pro-Auto-Filter-Bot-V2.o">😋Click Here</a></b>
+<b>☞ 📖Source Code</b> : <a href="https://t.me/comemanrepo/6">😋Click Here</a></b>
 """
 
-    TEAM = """Team [MoTech YT](t.me/Mo_Tech_YT)"""
+    TEAM = """Team [Achu vj](t.me/Sanoob_Achu_18)"""
